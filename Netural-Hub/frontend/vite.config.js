ec1abd9cd173e96e3f09729946224fab15949d69
DUA <hafsamemon920@gmail.com>
2026-06-17 12:00:30 -0700

    Fix vite config conflict

--- Netural-Hub/frontend/vite.config.js
+++ Netural-Hub/frontend/vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/AI-CARE-COORDINATION-SYSTEM/',
   plugins: [react()],
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/AI-CARE-COORDINATION-SYSTEM/',
-  plugins: [react()],
->>>>>>> a792f9a23b48e8bf403b4ff830952f77aa77e101
 })
